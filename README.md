71aff9edf27b6bd1a32737cc51872835f5a1e585
Ethan P <epini@sfu.ca>
2020-01-23 18:46:24 -0800

    docs: Updated developer documentation

--- README.md
+++ README.md
@@ -13,13 +13,14 @@ A wrapper around `cmake` and `make` is provided for convenience.
 ls target/[platform string]/[build type]
 ```
 
-| Option                                       | Description                                |
-| -------------------------------------------- | ------------------------------------------ |
-| `--debug`                                    | Creates a debug build.                     |
-| `--release`                                  | Creates a release build.                   |
-| `--reconfigure`                              | Reconfigures the project.                  |
-| `--toolchain=/path/to/cmake/toolchain.cmake` | Specifies a toolchain for cross-compiling. |
-| `--print-info`                               | Prints project and build information.      |
+| Option                                       | Description                                  |
+| -------------------------------------------- | -------------------------------------------- |
+| `--debug`                                    | Creates a debug build.                       |
+| `--release`                                  | Creates a release build.                     |
+| `--reconfigure`                              | Reconfigures the project.                    |
+| `--cores=4`                                  | Specifies the number of cores to build with. |
+| `--toolchain=/path/to/cmake/toolchain.cmake` | Specifies a toolchain for cross-compiling.   |
+| `--print-info`                               | Prints project and build information.        |
 
 The `--debug`, `--release`, and `--toolchain` options will be automatically applied to subsequent invocations of the wrapper.
 
--- scripts/cmake/common.cmake
+++ scripts/cmake/common.cmake
@@ -13,41 +13,52 @@ include("${CMAKE_CURRENT_LIST_DIR}/fn_export_headers.cmake")
 #     Creates a module target.
 #     This will generate a static library that can be re-used in other modules or executables.
 #
-function(arepa_create_module module_name export_prefix)
-	message("   --> Creates static library \"${module_name}\" (as \"${export_prefix}\")")
+# Arguments:
+#
+#     * `target_name`   - The target name.
+#     * `export_prefix` - The library's export prefix path.
+#                         If this is set to "example", headers would be included as "example/Header.hpp".
+#
+function(arepa_create_module target_name export_prefix)
+	message("   --> Creates static library \"${target_name}\" (as \"${export_prefix}\")")
 
 	# Create the build target.
-	add_library("${module_name}")
+	add_library("${target_name}")
 
 	# Configure the install command.
-	install(TARGETS "${module_name}"
+	install(TARGETS "${target_name}"
 		ARCHIVE DESTINATION lib
 		PUBLIC_HEADER DESTINATION "include/${export_prefix}"
 	)
 
 	# Set variables.
-	set_target_properties("${module_name}"
+	set_target_properties("${target_name}"
 		PROPERTIES
 			AREPA_HEADER_PREFIX "${export_prefix}"
 	)
 endfunction(arepa_create_module)
 
 
-# Function:    arepa_create_module
+# Function:    arepa_create_executable
 # Description:
 #
 #     Creates an executable target.
-#     This will generate a static library that can be re-used in other modules or executables.
+#     This will create an executable file.
 #
-function(arepa_create_executable module_name executable_name)
+# Arguments:
+#
+#     * `target_name`     - The target name.
+#     * `executable_name` - The output filename for the executable.
+#
+function(arepa_create_executable target_name executable_name)
 	message("   --> Creates executable \"${executable_name}\"")
 
 	# Create the build target.
-	add_executable("${module_name}")
-	set_target_properties(${module_name} PROPERTIES OUTPUT_NAME "${executable_name}")
+	add_executable("${target_name}")
+	set_target_properties(${target_name} PROPERTIES OUTPUT_NAME "${executable_name}")
 
 	# Configure the install command.
-	install(TARGETS "${module_name}"
+	install(TARGETS "${target_name}"
 		RUNTIME DESTINATION bin
 	)
 endfunction(arepa_create_executable)
