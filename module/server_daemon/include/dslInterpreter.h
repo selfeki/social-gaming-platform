63f1b10dc21a2611a39adefc845cd56df9f3e320
msg13 <msg@sfu.ca>
2020-01-31 18:19:14 -0800

    Dev: Create Dsl Interpreter .h file

---
+++ module/server_daemon/include/dslInterpreter.h
@@ -0,0 +1,19 @@
+#ifndef dslInterpreter
+#define dslInterpreter 
+
+
+class dslInterpreter {
+
+public:
+	dslInterpreter() ;
+	getNewState(State& state) ;
+
+
+
+private:
+  State state;
+
+
+};
+
+#endif 
