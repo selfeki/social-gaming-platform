493252aecda013ee45a6d4c953b34ff67addc1b1
selfeki <samielfeki@hotmail.com>
2020-01-31 18:47:10 -0800

    Dev: add template for rules

--- module/server_daemon/include/dslInterpreter.h
+++ module/server_daemon/include/dslInterpreter.h
@@ -1,19 +1,53 @@
-#ifndef dslInterpreter
-#define dslInterpreter 
+#ifndef DSLInterpreter
+#define DSLInterpreter 
+
+#include <vector>
 
 
-class dslInterpreter {
+enum RuleKeyWord {
+  FOR_EACH,
+  LOOP,
+  IN_PARALLEL,
+  PARALLEL_FOR,
+  SWITCH,
+  WHEN,
+  EXTEND,
+  REVERSE,
+  SHUFFLE,
+  SORT,
+  DEAL,
+  DISCARD,
+  ADD,
+  TIMER,
+  INPUT_CHOICE,
+  INPUT_TEXT,
+  INPUT_VOTE,
+  MESSAGE,
+  GLOBAL_MESSAGE,
+  SCORES
+};
 
+
+class State;
+
+
+class Rule {
 public:
-	dslInterpreter() ;
-	getNewState(State& state) ;
-
-
 
 private:
+  std::vector<Rule> children;
+
+};
+
+
+class DSLInterpreter {
+public:
+	State getNewState(State& state);
+
+private:
+  interpretRule()
+  interpretGlobalMessage(Rule)
   State state;
-
-
 };
 
 #endif 
