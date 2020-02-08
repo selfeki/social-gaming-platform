acbeec136399e3901984d1975f14d01a08f2bb09
hceong <hceong@sfu.ca>
2020-02-07 17:43:47 -0800

    fixed

--- module/server_daemon/src/chatserver.cpp
+++ module/server_daemon/src/chatserver.cpp
@@ -117,7 +117,7 @@ std::vector<messageReturnAlias> parseCommandAndCollectResponse(const std::string
     }
   }
   else if(tokens[0] == "/kick"){
-    game_manager_message = game_manager.kickPlayerCommand(id, tokens[1]);
+    game_manager_message = game_manager.kickPlayerCommand(id);
   }
   else if(tokens[0] == "/quit"){
     game_manager_message = game_manager.leaveRoomCommand(id);
