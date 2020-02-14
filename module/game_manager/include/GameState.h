0db340d974e5dfddd4a9789e1d81237ef834f2ba
selfeki <selfeki@sfu.ca>
2020-02-13 20:22:41 -0800

    separate MapWrapper into its own file

--- module/game_manager/include/GameState.h
+++ module/game_manager/include/GameState.h
@@ -4,36 +4,14 @@
 #include <iostream>
 #include <unordered_map>
 
-// todo: implement State struct
-
-template <class keyTy, class valTy>
-struct MapWrapper {
-public:
-    void
-    set(keyTy key, valTy value) {
-        assignments[key] = value;
-    }
-
-    valTy
-    get(const keyTy& key) const {
-        auto found = assignments.find(key);
-        if (found != assignments.end()) {
-            return { found->second };
-        } else {
-            std::cerr << "Invalid key: " << key << '\n';
-            throw;
-        }
-    }
-
-private:
-    std::unordered_map<keyTy, valTy> assignments;
-};
-
+#include "MapWrapper.h"
 
 ////// In-Game State
 
-using MapValue = std::unordered_map<std::string, boost::recursive_variant_>;
+using MapValue = MapWrapper<std::string, boost::recursive_variant_>;
 using ValueList = std::vector<boost::recursive_variant_>;
+
+// todo: duplicate DS, share this variant with the Rule module
 
 using EnvValue = boost::make_recursive_variant<
     MapValue,
---
+++ module/game_manager/include/MapWrapper.h
@@ -0,0 +1,25 @@
+// Todo: replace std::unordered_map with Abseil flat_hash_map
+#include <unordered_map>
+
+template <class keyTy, class valTy>
+struct MapWrapper {
+public:
+    void
+    set(keyTy key, valTy value) {
+        assignments[key] = value;
+    }
+
+    valTy
+    get(const keyTy& key) const {
+        auto found = assignments.find(key);
+        if (found != assignments.end()) {
+            return { found->second };
+        } else {
+            std::cerr << "Invalid key: " << key << '\n';
+            throw;
+        }
+    }
+
+private:
+    std::unordered_map<keyTy, valTy> assignments;
+};
