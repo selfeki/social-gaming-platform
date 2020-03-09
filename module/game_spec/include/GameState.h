6d7fb4e3ab41a0e72c4b794fd86e7ffbc33b7a89
selfeki <selfeki@sfu.ca>
2020-03-09 15:27:44 -0700

    add message related functionality to GameState

--- module/game_spec/include/GameState.h
+++ module/game_spec/include/GameState.h
@@ -8,14 +8,19 @@ namespace gameSpecification {
 
 // A GameState is the input and output of the DSLInterpreter
 
+using GameMessage = std::string;
+
+using uniqueName = std::string;
+
 struct GamePlayer {
-    std::string name;
-    std::vector<std::string> messages;
+    uniqueName name;
+    std::vector<GameMessage> messages;
+
 };
 
 struct GameAudience {
-    std::string name;
-    std::vector<std::string> messages;
+    uniqueName name;
+    std::vector<GameMessage> messages;
 };
 
 using Environment = MapWrapper<std::string, Expression>;
@@ -30,6 +35,39 @@ struct GameState {
     // A user might have game-agnostic data
     std::vector<GamePlayer> players;
     std::vector<GameAudience> audience;
+
+    void enqueueMessage(uniqueName name, GameMessage message) {
+        auto it = std::find_if(players.begin(), players.end(),
+            [&name](const auto& player) { return player.name == name; }
+        );
+        if (it != players.end()) { 
+            it->messages.push_back(message);
+            return;
+        }
+        it = std::find_if(players.begin(), players.end(),
+            [&name](const auto& player) { return player.name == name; }
+        );
+        if (it != players.end()) { 
+            it->messages.push_back(message);
+        }
+    }
+
+    void enqueuePlayersMessage(GameMessage message) {
+        for (const auto& player: players) {
+            enqueueMessage(player.name, message);
+        }
+    }
+
+    void enqueueAudienceMessage(GameMessage message) {
+        for (const auto& aud: audience) {
+            enqueueMessage(aud.name, message);
+        }
+    }
+
+    void enqueueGlobalMessage(GameMessage message) {
+        enqueuePlayersMessage(message);
+        enqueueAudienceMessage(message);
+    }
 };
 
 
