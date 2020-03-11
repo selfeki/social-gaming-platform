cbb5b0be6f8b60d5550fe73978eb73d4a69f9ab5
Ethan P <epini@sfu.ca>
2020-03-10 22:12:02 -0700

    common: Fix missing include for Util functions

--- module/common/include/Util.hpp
+++ module/common/include/Util.hpp
@@ -2,8 +2,28 @@
 
 #include <functional>
 #include <vector>
+#include <optional>
 
 namespace arepa {
+
+/**
+ * Searches for something in a map, returning an optional based on whether or not it was found.
+ * This is a useful replacement.
+ *
+ * @tparam T The type.
+ * @param vec The vector.
+ * @param lambda The search lambda.
+ * @return An optional of what was found.
+ */
+template <typename V, typename K, typename _Map>
+constexpr std::optional<V> find_in_map(const _Map& map, const K& key) {
+    auto find = map.find(key);
+    if (find == map.end()) {
+        return std::nullopt;
+    } else {
+        return { find->second };
+    }
+}
 
 /**
  * Searches for something in a vector, returning an optional based on whether or not it was found.
@@ -15,7 +35,7 @@ namespace arepa {
  * @return An optional of what was found.
  */
 template <typename T, typename Predicate>
-std::optional<T> find_if(const std::vector<T>& vec, Predicate lambda) {
+constexpr std::optional<T> find_if(const std::vector<T>& vec, Predicate lambda) {
     auto find = std::find_if(vec.begin(), vec.end(), lambda);
     if (find == vec.end()) {
         return std::nullopt;
