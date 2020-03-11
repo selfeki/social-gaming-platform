66c3b2aabf77f1f8c2461980daf0d819c35f1791
Ethan P <epini@sfu.ca>
2020-03-10 23:52:10 -0700

    server: Add packet_type alias to Client

--- module/server/include/Client.hpp
+++ module/server/include/Client.hpp
@@ -11,11 +11,16 @@ namespace arepa::server {
  * A container struct that holds a client connection, and a client packet queue.
  */
 class Client {
+#pragma mark - Types -
+public:
+    using packet_type = arepa::protocol::Packet;
+
 
 #pragma mark - Fields (Public) -
 public:
     std::shared_ptr<Connection> connection;
-    std::shared_ptr<arepa::communication::ChannelSingleQueue<arepa::protocol::Packet>> messages;    // NOTE: This is a pointer.
+    std::shared_ptr<arepa::communication::ChannelSingleQueue<packet_type>> messages;    // NOTE: This is a pointer.
+
 
 #pragma mark - Constructors -
 public:
