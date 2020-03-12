68edc67181e5385595aed4a0981dffc2061b76ca
selfeki <selfeki@sfu.ca>
2020-03-12 11:26:35 -0700

    implement GameInstance

--- module/game_manager/include/GameInstance.h
+++ module/game_manager/include/GameInstance.h
@@ -4,10 +4,17 @@
 
 using namespace gameSpecification;
 using rule::InterpretVisitor;
+using rule::Rule;
+using rule::RuleList;
 
 class GameInstance {
 public:
-    GameInstance(const rule::RuleList& rules);
+    GameInstance(GameState state, const RuleList& rs)
+        : gameState { state }
+        , interpreter { gameState }
+        , rules { rs }
+        , ruleInd { 0 }
+        , isTerminated { false } {}
 
     // Interprets rules until requires user interaction or game ends.
     // It updates passed in state based upon interpretation of the current rule.
@@ -15,13 +22,22 @@ public:
     // todo: include fields in state specifying game conclusion or user input request
     // todo: include artificial rule at end of rule list representing game conclusion?
     void
-    updateState();
+    updateState() {
+        if (ruleInd >= rules.size()) {
+            isTerminated = true;
+            return;
+        }
+        rules[ruleInd].accept(interpreter);
+        ruleInd++;
+    }
 
     GameState&
-    getGameState();
-
+    getGameState() { return gameState; }
 
 private:
-    //   GameState&      gameState;
+    GameState gameState;
     InterpretVisitor interpreter;
+    const RuleList& rules;
+    std::size_t ruleInd;
+    bool isTerminated;
 };
