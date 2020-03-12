df54a1b73f460ed487a84ce1de317ef4df5a7505
jzl1 <justin_liu_2@sfu.ca>
2020-03-11 23:57:15 -0700

    added validFieldValues for json validator. updated requirements to maps over vector

--- module/serializer/include/jsonSerializer.h
+++ module/serializer/include/jsonSerializer.h
@@ -64,11 +64,28 @@ bool hasAllRequiredFields(const json&);
 
 bool hasNoExtraFields(const json&);
 
+bool validFieldValues(const json& j);
+
 //json configuration requirements:
-//used by hasAllRequiredFields() and hasNoExtraFields() to validate json fields
-const std::vector<std::string> FIELDS = {"configuration","constants","variables","per-player","per-audience","rules"};
-const std::vector<std::string> FIELDS_configuration = {"name", "player count"};
-const std::vector<std::string> FIELDS_playerCount = {"min","max"};
+//used by hasAllRequiredFields(), hasNoExtraFields(), and validFieldValues() to validate json fields
+const std::map<std::string,json::value_t> FIELDS = {
+												{"configuration", json::value_t::object},
+												{"constants", json::value_t::array},
+												{"variables", json::value_t::array},
+												{"per-player", json::value_t::array},
+												{"per-audience", json::value_t::array},
+												{"rules", json::value_t::array}
+												};
+const std::map<std::string,json::value_t> FIELDS_configuration = {
+												{"name", json::value_t::string},
+												{"player count", json::value_t::object},
+												{"audience", json::value_t::boolean},
+												{"setup", json::value_t::array}
+												};
+const std::map<std::string,json::value_t> FIELDS_playerCount = {
+												{"min", json::value_t::number_integer},
+												{"max", json::value_t::number_integer},
+												};
 
 // enum FIELDS {
 //     CONF,
