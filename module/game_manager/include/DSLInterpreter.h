48c406c6f10437e7806c454a7b02c7c48dbe59bb
selfeki <selfeki@sfu.ca>
2020-03-12 19:37:19 -0700

    implement ObjectPointer

--- module/game_manager/include/DSLInterpreter.h
+++ module/game_manager/include/DSLInterpreter.h
@@ -7,6 +7,43 @@
 
 namespace gameSpecification::rule {
 
+struct ObjectPointer {
+    std::vector<std::string_view> indices;
+
+    // I'm not sure if this is gonna work, needs testing
+    // I'm trying to dereference the pointer
+    // https://stackoverflow.com/questions/550859/reference-to-value-of-stl-map-element
+
+    std::optional<ExpMap>
+    getExpMap(const Expression& exp) {
+        const ExpMap* res;
+        try {
+            res = &boost::get<ExpMap>(exp);
+        } catch(boost::bad_get e) {
+            return { };
+         }
+        return *res;
+    }
+
+    std::optional<Expression>
+    dereferenceFromContext(const ExpMap& expMap) {
+        Expression exp = expMap;
+        for (const auto& ind : indices) {
+            auto nestedMap = getExpMap(exp);
+            if (!nestedMap) {
+                return { };
+            }
+            auto m = nestedMap.value().map;
+            auto it = m.find(ind);
+            if (it == m.end()) { 
+                return { };
+            }
+            exp = it->second;
+        }
+        return exp;
+    }
+};
+
 class InterpretVisitor : public RuleVisitor {
 public:
     InterpretVisitor(GameState& state)
@@ -27,7 +64,7 @@ private:
 
     bool needUserInput;
 
-    std::stack<const Rule*> scope;
+    std::stack<const Rule*> ruleStack;
 };
 
 }    // namespace gameSpecification::rule
