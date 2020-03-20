2484b75291674b5d4d95efa528fc30ccefcc2098
selfeki <selfeki@sfu.ca>
2020-03-19 20:52:27 -0700

    update messageQueue design in GameState

--- module/game_spec/include/GameState.h
+++ module/game_spec/include/GameState.h
@@ -13,19 +13,14 @@ namespace gameSpecification {
 
 // A GameState is the input and output of the DSLInterpreter
 
-using GameMessage = std::string_view;
-
 using uniqueName = std::string_view;
 
-struct GamePlayer {
-    uniqueName name;
-    std::vector<GameMessage> messages;
+
+struct GameMessage {
+    uniqueName targetUser;
+    std::string_view content;
 };
 
-struct GameAudience {
-    uniqueName name;
-    std::vector<GameMessage> messages;
-};
 
 struct InputRequest {
     uniqueName targetUser;
@@ -35,64 +30,51 @@ struct InputRequest {
     std::optional<int> timeout;
 };
 
+// keeping this for legacy code
 using Environment = ExpMap;
 
 struct GameState {
+    ExpMap constants;
+    ExpMap variables;
+    ExpMap perPlayer;
+    ExpMap perAudience;
 
+    // keeps track of local variables
+    std::vector<ExpMap> context;
 
-    Environment constants;
-    Environment variables;
-    Environment perPlayer;
-    Environment perAudience;
-    // keeps track of which variables are in scope
-    std::stack<Environment> context;
-
+    // Design: should these 2 be combined?
     // stores input requests to be delivered to users
     std::vector<InputRequest> inputRequests;
+    std::vector<GameMessage>  messageQueue;
 
-    // player exclusive data contained here or in userStates?
-    // A user might have game-agnostic data
-    std::vector<GamePlayer> players;
-    std::vector<GameAudience> audience;
-
-
-    void enterScope() {
-        context.emplace();
+    void 
+    enterScope() {
+        context.emplace_back();
     }
 
-    void exitScope() {
-        context.pop();
+    void 
+    exitScope() {
+        context.pop_back();
     }
 
-    void enqueueMessage(uniqueName name, GameMessage message) {
-        auto it = std::find_if(players.begin(), players.end(),
-            [&name](const auto& player) { return player.name == name; });
-        if (it != players.end()) {
-            it->messages.push_back(message);
-            return;
+    void
+    enqueueMessage(uniqueName name, std::string_view message) {
+        // todo: check if name exists in the game
+        messageQueue.push_back({name, message});
+    }
+
+    void
+    enqueueGlobalMessage(std::string_view message) {
+        for (auto userTy : {"players", "audience"}) {
+            auto users = castExp<ExpList>(variables.map[userTy]);
+            for (const auto& exp : users.list) {
+                auto userExpMap = castExp<ExpMap>(exp);
+                assert(user.count("name") > 0);
+                auto nameExp = userExpMap.map["name"];
+                auto name = castExp<std::string_view>(nameExp);
+                enqueueMessage(name, message);
+            }
         }
-        it = std::find_if(players.begin(), players.end(),
-            [&name](const auto& player) { return player.name == name; });
-        if (it != players.end()) {
-            it->messages.push_back(message);
-        }
-    }
-
-    void enqueuePlayersMessage(GameMessage message) {
-        for (const auto& player : players) {
-            enqueueMessage(player.name, message);
-        }
-    }
-
-    void enqueueAudienceMessage(GameMessage message) {
-        for (const auto& aud : audience) {
-            enqueueMessage(aud.name, message);
-        }
-    }
-
-    void enqueueGlobalMessage(GameMessage message) {
-        enqueuePlayersMessage(message);
-        enqueueAudienceMessage(message);
     }
 };
 
